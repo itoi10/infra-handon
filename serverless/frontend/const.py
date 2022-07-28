2610af4188433163734d645a009da7314b86431e
itict10 <itict10@gmail.com>
2022-07-28 22:23:08 +0900

    デプロイ

--- serverless/frontend/const.py
+++ serverless/frontend/const.py
@@ -3,4 +3,5 @@
 # ----------------
 
 # APIのベースURL
-BASE_URL = "http://127.0.0.1:8000/"
+# BASE_URL = "http://127.0.0.1:8000/"
+BASE_URL = "https://usres8iga1.execute-api.ap-northeast-1.amazonaws.com/api/"
