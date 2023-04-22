9e11942cf140a71018ff0b0529d9feba2eb14ace
itict10 <itict10@gmail.com>
2023-04-22 13:11:41 +0900

    pulumi update s3

--- pulumi/aws-python/__main__.py
+++ pulumi/aws-python/__main__.py
@@ -1,10 +1,46 @@
 """An AWS Python Pulumi program"""
 
-import pulumi
 from pulumi_aws import s3
 
+import pulumi
+
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = s3.Bucket(
+    "my-bucket",
+    # index.htmlをwebsiteのindexとして設定
+    website=s3.BucketWebsiteArgs(
+        index_document="index.html",
+    ),
+)
+
+# S3バケットの所有権の設定
+ownership_controls = s3.BucketOwnershipControls(
+    "ownership-controls",
+    bucket=bucket.id,
+    rule=s3.BucketOwnershipControlsRuleArgs(
+        object_ownership="ObjectWriter",
+    ),
+)
+
+# S3バケットの公開アクセスを許可する
+public_access_block = s3.BucketPublicAccessBlock(
+    "public-access-block",
+    bucket=bucket.id,
+    block_public_acls=False,
+)
+
+# Create a Pulumi resource (AWS S3 Bucket Object)
+bucket_object = s3.BucketObject(
+    "index.html",
+    bucket=bucket.id,
+    source=pulumi.FileAsset("index.html"),
+    content_type="text/html",
+    acl="public-read",
+    opts=pulumi.ResourceOptions(depends_on=[public_access_block]),
+)
 
 # Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+pulumi.export(
+    "bucket_endpoint",
+    pulumi.Output.concat("http://", bucket.website_endpoint),
+)
